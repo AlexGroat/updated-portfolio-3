f1cf6d4583aceb9993bf5b094706dc8954acaf06
Alex Groat <alex.groat.95@gmail.com>
2022-01-01 12:05:49 +1030

    update

--- client/src/PortfolioContainer/Home/Profile.js
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -6,10 +6,10 @@ export default function Profile() {
             <div className='profile-parent'>
                 <div className='profile-details'>
                     <div className='colz'>
-                        <a href='#'>
+                        <a href='https://github.com/AlexGroat'>
                             <i className='fa fa-github-square'></i>
                         </a>
-                        <a href='#'>
+                        <a href='https://www.linkedin.com/in/alex-groat-0a382513b/'>
                             <i className='fa fa-linkedin-square'></i>
                         </a>
                     </div>
