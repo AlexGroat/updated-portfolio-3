3aab5a9bb85b0d3dc7af13e6429fc40dee6e4997
Alex Groat <alex.groat.95@gmail.com>
2022-01-02 23:38:34 +1030

    about me added before styling

--- client/src/PortfolioContainer/AboutMe/AboutMe.js
+++ client/src/PortfolioContainer/AboutMe/AboutMe.js
@@ -13,10 +13,53 @@ export default function AboutMe(props) {
 
     const fadeInSubscription = ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
 
+    const SCREEN_CONSTANTS = {
+        description: "Full stack developer undertaking the University of Adelaide's Coding Bootcamp. Strong passion for building front and back end operating applications.",
+        highlights: {
+            bullets: [
+                "Full Stack Development",
+                "React and React Native",
+                "Interactive Design",
+                "Building REST API",
+                "Database Construction",
+            ],
+            heading: "A few of my skills are:"
+        }
+    }
+
+    const renderHighlight = () => {
+        return (
+            SCREEN_CONSTANTS.highlights.bullets.map((value, i) => (
+                <div className='highlight' key={i}>
+                    <div className='highlight-blob'></div>
+                <span>{value}</span>
+                </div>
+            ))
+        )
+    }
+
     return (
         <div className='about-me-container screen-container' id={props.id || ""}>
             <div className='about-me-parent'>
                 <ScreenHeading title={'About Me'} subHeading={'Why Choose Me?'}/>
+                <div className='about-me-card'>
+                    <div className='about-me-profile'></div>
+                    <div className='about-me-details'>
+                        <span className='about-me-description'>{SCREEN_CONSTANTS.description}</span>
+                        <div className='about-me-highlights'>
+                            <div className='highlight-heading'>
+                                <span>{SCREEN_CONSTANTS.highlights.heading}</span>
+                            </div>
+                            {renderHighlight()}
+                        </div>
+                        <div className='about-me-options'>
+                        <button className="btn primary-btn"> Want to know more? </button>
+                             <a href="myresume.pdf" download="Alex Groat myresume.pdf">
+                            <button className="btn highlighted-btn">Get Resume</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
 
         </div>
