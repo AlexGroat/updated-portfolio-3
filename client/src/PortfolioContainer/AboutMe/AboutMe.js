1354afff30089f5fb66da4c4e6106f6fd4238339
Alex Groat <alex.groat.95@gmail.com>
2022-01-02 23:10:26 +1030

    working scroll to about me

--- client/src/PortfolioContainer/AboutMe/AboutMe.js
+++ client/src/PortfolioContainer/AboutMe/AboutMe.js
@@ -2,14 +2,23 @@ import React, {useEffect} from 'react';
 import ScreenHeading from '../../utilities/ScreenHeading/ScreenHeading';
 import ScrollService from '../../utilities/ScrollService';
 import Animations from '../../utilities/Animations';
+import './AboutMe.css';
 
 export default function AboutMe(props) {
+    let fadeInScreenHandler = (screen) => {
+        if (screen.fadeInScreen != props.id)
+        return
+        Animations.animations.fadeInScreen(props.id);
+    }
+
+    const fadeInSubscription = ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
+
     return (
-        <div className='about-me-container screen-container'>
+        <div className='about-me-container screen-container' id={props.id || ""}>
             <div className='about-me-parent'>
                 <ScreenHeading title={'About Me'} subHeading={'Why Choose Me?'}/>
             </div>
-        
+
         </div>
     )
 }
