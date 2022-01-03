aa7fcb0e287877217015fe11e1693fd78fd1df6e
Alex Groat <alex.groat.95@gmail.com>
2022-01-03 23:45:15 +1030

    Form style complete

--- client/src/PortfolioContainer/ContactMe/ContactMe.js
+++ client/src/PortfolioContainer/ContactMe/ContactMe.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import Typical from "react-typical";
-
-
+import axios from "axios";
+import { toast } from "react-toastify";
 
 import imgBack from '../../../src/images/mailz.jpeg';
 import load1 from '../../../src/images/load2.gif';
@@ -39,6 +39,38 @@ export default function ContactMe(props) {
 
       console.log(name)
 
+      const submitForm = async (e) => {
+        e.preventDefault();
+        try {
+          let data = {
+            name,
+            email,
+            message,
+          };
+
+          console.log(data)
+
+          setBool(true);
+          const res = await axios.post(`/contact`, data);
+          if (name.length === 0 || email.length === 0 || message.length === 0) {
+            setBanner(res.data.msg);
+            toast.error(res.data.msg);
+            setBool(false);
+          } else if (res.status === 200) {
+            setBanner(res.data.msg);
+            toast.success(res.data.msg);
+            setBool(false);
+    
+            setName("");
+            setEmail("");
+            setMessage("");
+          }
+        } catch (error) {
+          console.log(error);
+        }
+        
+      };
+
     return (
         <div className='main-container' id={props.id || ''}>
             <ScreenHeading 
@@ -74,7 +106,7 @@ export default function ContactMe(props) {
                         <img src={imgBack} alt='image not found'/>
                     </div>
 
-                    <form>
+                    <form onSubmit={submitForm}>
                         <p>{banner}</p>
 
                         <label htmlFor='name'>Name</label>
@@ -96,6 +128,9 @@ export default function ContactMe(props) {
                         <div className='send-btn'>
                             <button type='submit'>
                                 send<i className='fa fa-paper-plane'/>
+                                {bool?(<b className='load'>
+                                    <img src={load1} al='image not responding'/>
+                                </b>) : ("")}
                             </button>
                         </div>
                     </form>
